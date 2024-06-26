61c68d281878f51b8dd5bdbafe2740af86ab6371
hobs <git@totalgood.com>
2024-06-26 14:28:28 -0700

    model.pth moved, added nonworking transformer

--- music_train.py
+++ music_train.py
@@ -8,6 +8,9 @@ import sys
 
 
 gpu_available = torch.cuda.is_available()
+device = torch.device('cpu')
+if gpu_available:
+    device = torch.device('cuda')
 
 
 def split_into_batches(data, batch_size, seq_length):
@@ -37,6 +40,113 @@ def split_into_batches(data, batch_size, seq_length):
         yield x, y
 
 
+class musicTransformer(nn.Module):
+    def __init__(self, vocab, n_hidden=512, n_layers=1, lr=0.001, dropout_prob=0.4):
+        super().__init__()
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden  # nhead
+        self.dropout_prob = dropout_prob
+        self.lr = lr
+
+        self.vocab = vocab
+        self.int2char = dict(enumerate(self.vocab))
+        self.char2int = {ch: ii for ii, ch in self.int2char.items()}
+
+        """ nn.TransformerDecoderLayer
+        d_model (int) – the number of expected features in the input (required).
+        nhead (int) – the number of heads in the multiheadattention models (required).
+        dim_feedforward (int) – the dimension of the feedforward network model (default=2048).
+        dropout (float) – the dropout value (default=0.1).
+        activation (Union[str, Callable[[Tensor], Tensor]]):
+            the activation function of the intermediate layer, can be a string (“relu” or “gelu”) or a unary callable. Default: relu
+        layer_norm_eps (float) – the eps value in layer normalization components
+            (default=1e-5).
+        batch_first (bool):
+            If True, then the input and output tensors are provided as (batch, seq, feature).
+            Default: False (seq, batch, feature).
+        norm_first (bool): if True, layer norm is done prior to self attention,
+            multihead attention and feedforward operations, respectively.
+            Otherwise it’s done after.
+            Default: False (after).
+        bias (bool) – If set to False, Linear and LayerNorm layers will not learn an additive bias. Default: True.
+        """
+        # TODO: use Karpathy miniGPT architecture - https://github.com/karpathy/minGPT/blob/master/mingpt/model.py
+        # self.transformer = nn.TransformerDecoderLayer(
+        #     d_model=len(self.vocab),
+        #     nhead=4)
+
+        """ nn.Transformer()
+        d_model (int) – the number of expected features in the encoder/decoder inputs (default=512).
+        nhead (int) – the number of heads in the multiheadattention models (default=8).
+        num_encoder_layers (int) – the number of sub-encoder-layers in the encoder (default=6).
+        num_decoder_layers (int) – the number of sub-decoder-layers in the decoder (default=6).
+        dim_feedforward (int) – the dimension of the feedforward network model (default=2048).
+        dropout (float) – the dropout value (default=0.1).
+        activation (Union[str, Callable[[Tensor], Tensor]]):
+            the activation function of encoder/decoder intermediate layer, can be a string (“relu” or “gelu”) or a unary callable.
+            Default: relu
+        custom_encoder (Optional[Any]):
+            custom encoder (default=None).
+        custom_decoder (Optional[Any]): custom decoder (default=None).
+        layer_norm_eps (float): the eps value in layer normalization components (default=1e-5).
+        batch_first (bool): True=then the input and output tensors are provided as (batch, seq, feature).
+            Default: False (seq, batch, feature).
+        norm_first (bool):
+            True: encoder and decoder layers will perform LayerNorms before other attention and feedforward operations, otherwise after.
+            Default: False (after).
+        bias (bool): If set to False, Linear and LayerNorm layers will not learn an additive bias. Default: True.
+        """
+        self.transformer = nn.Transformer(
+            d_model=len(self.vocab),
+            nhead=self.n_hidden,
+            num_encoder_layers=self.n_layers,
+            num_decoder_layers=self.n_layers
+        )
+        """ LSTM layer
+        number of tokens in vocab = number of features of input
+        n_hidden = number of lstm units per layer
+        n_layers = number of hidden layers
+        """
+        # self.lstm = nn.LSTM(len(self.vocab), n_hidden, n_layers,
+        #                     dropout_prob=dropout_prob, batch_first=True)
+        # dropout layer
+        self.dropout = nn.Dropout(dropout_prob)
+        """
+        output layer
+        applies y = xA ^ T + b
+        """
+        self.fc = nn.Linear(self.n_hidden, len(self.vocab))
+
+    def forward(self, x, hidden_init):
+        transformer_out, hidden_state = self.transformer(x, hidden_init)
+        dropout_out = self.dropout(transformer_out)
+
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        dropout_out = dropout_out.contiguous().view(-1, self.n_hidden)
+
+        # last layer
+        out = self.fc(dropout_out)
+
+        # return final output and hidden_state
+        return out, hidden_state
+
+    def init_hidden(self, batch_size):
+        """LSTM's have two separate states(hidden and memory)
+        denoted as state_h and state_c respectively which is why to sets of weights are initialised
+        """
+        weights = next(self.parameters()).data
+        if (gpu_available):
+            hidden = (
+                weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(device),
+                weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(device)
+            )
+        else:
+            hidden = (weights.new(self.n_layers, batch_size, self.n_hidden).zero_(),
+                      weights.new(self.n_layers, batch_size, self.n_hidden).zero_())
+
+        return hidden
+
+
 class musicRNN(nn.Module):
     def __init__(self, vocab, n_hidden=512, n_layers=3, lr=0.001, dropout_prob=0.4):
         super().__init__()
@@ -55,15 +165,17 @@ class musicRNN(nn.Module):
         n_hidden = number of lstm units per layer
         n_layers = number of hidden layers
         """
-        self.lstm = nn.LSTM(len(self.tokens), n_hidden, n_layers,
-                            dropout=dropout_prob, batch_first=True)
+        self.lstm = nn.LSTM(
+            len(self.tokens), n_hidden, n_layers,
+            dropout_prob=dropout_prob, batch_first=True
+        )
 
         # dropout layer
         self.dropout = nn.Dropout(dropout_prob)
 
         """
         output layer
-        applies y = xA^T + b
+        applies y = xA ^ T + b
         """
         self.fc = nn.Linear(n_hidden, len(self.tokens))
 
@@ -83,14 +195,15 @@ class musicRNN(nn.Module):
         return out, hidden_state
 
     def init_hidden(self, batch_size):
-        """
-        LSTM's have two separate states (hidden and memory) 
+        """LSTM's have two separate states(hidden and memory)
         denoted as state_h and state_c respectively which is why to sets of weights are initialised
         """
         weights = next(self.parameters()).data
         if (gpu_available):
-            hidden = (weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(),
-                  weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda())
+            hidden = (
+                weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(device),
+                weights.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(device)
+            )
         else:
             hidden = (weights.new(self.n_layers, batch_size, self.n_hidden).zero_(),
                       weights.new(self.n_layers, batch_size, self.n_hidden).zero_())
@@ -214,7 +327,9 @@ def main(argv):
 
     int_encoded = np.array([char2int[ch] for ch in data])
 
-    model = musicRNN(vocab, conf.n_hidden, conf.n_layers, conf.lr, conf.dropout)
+    # model = musicTransformer(vocab, conf.n_hidden, conf.n_layers, conf.lr, conf.dropout)
+
+    model = musicTransformer(vocab, n_hidden=conf.n_hidden, n_layers=1, lr=conf.lr, dropout_prob=conf.dropout)
     print(model)
     train(model, int_encoded, vocab, epochs=conf.n_epochs, batch_size=conf.batch_size,
           seq_length=conf.seq_length, clip=conf.grad_clip, lr=conf.lr,
